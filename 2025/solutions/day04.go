72758b4d613b747a6902078c6539835e88f4a462
Sean Malloy <sfmalloy@outlook.com>
2025-12-04 10:12:47 -0500

    feat(2025): slower map-based solution

--- 2025/solutions/day04.go
+++ 2025/solutions/day04.go
@@ -7,91 +7,84 @@ import (
 
 type Day04 struct{}
 
-func (d Day04) Parse(file *os.File) ([][]byte, error) {
+func (d Day04) Parse(file *os.File) (map[complex128]byte, error) {
 	data, err := io.ReadAll(file)
 	if err != nil {
 		return nil, err
 	}
-	lines := make([][]byte, 0)
-	row := make([]byte, 0)
-	for _, char := range data {
+	R := 0
+	for col, char := range data {
 		if char == '\n' {
-			lines = append(lines, row)
-			row = make([]byte, 0)
-		} else {
-			row = append(row, char)
+			R = col
+			break
 		}
 	}
-	return lines, nil
+	floor := make(map[complex128]byte)
+	row := 0
+	col := 0
+	for _, char := range data {
+		if char == '\n' {
+			continue
+		}
+		floor[complex(float64(row), float64(col%R))] = char
+		col += 1
+		if col > 0 && col%R == 0 {
+			row += 1
+		}
+	}
+	return floor, nil
 }
 
-func (d Day04) Part1(lines [][]byte) int {
+func (d Day04) Part1(lines map[complex128]byte) int {
 	adjacent := [8]complex128{-1 - 1i, 0 - 1i, 1 - 1i, -1, 1, -1 + 1i, 0 + 1i, 1 + 1i}
 
 	count := 0
-	for r, row := range lines {
-		for c, col := range row {
-			if col != '@' {
-				continue
+	for pos, col := range lines {
+		if col != '@' {
+			continue
+		}
+		found := 0
+		for _, delta := range adjacent {
+			neighbor := pos + delta
+			if lines[neighbor] == '@' {
+				found += 1
 			}
-			pos := complex(float64(r), float64(c))
-			found := 0
-			for _, delta := range adjacent {
-				neighbor := pos + delta
-				R := int(real(neighbor))
-				C := int(imag(neighbor))
-				if R < 0 || R >= len(lines) || C < 0 || C >= len(lines[R]) {
-					continue
-				}
-				if lines[R][C] == '@' {
-					found += 1
-				}
-			}
-			if found < 4 {
-				count += 1
-			}
+		}
+		if found < 4 {
+			count += 1
 		}
 	}
 	return count
 }
 
-func (d Day04) Part2(lines [][]byte) int {
+func (d Day04) Part2(lines map[complex128]byte) int {
 	adjacent := [8]complex128{-1 - 1i, 0 - 1i, 1 - 1i, -1, 1, -1 + 1i, 0 + 1i, 1 + 1i}
 
 	count := 0
 	for {
 		removable := make([]complex128, 0)
-		for r, row := range lines {
-			for c, col := range row {
-				if col != '@' {
-					continue
+
+		for pos, col := range lines {
+			if col != '@' {
+				continue
+			}
+			found := 0
+			for _, delta := range adjacent {
+				neighbor := pos + delta
+				if lines[neighbor] == '@' {
+					found += 1
 				}
-				pos := complex(float64(r), float64(c))
-				found := 0
-				for _, delta := range adjacent {
-					neighbor := pos + delta
-					R := int(real(neighbor))
-					C := int(imag(neighbor))
-					if R < 0 || R >= len(lines) || C < 0 || C >= len(lines[R]) {
-						continue
-					}
-					if lines[R][C] == '@' {
-						found += 1
-					}
-				}
-				if found < 4 {
-					removable = append(removable, pos)
-					count += 1
-				}
+			}
+			if found < 4 {
+				removable = append(removable, pos)
+				count += 1
 			}
 		}
 		if len(removable) == 0 {
 			return count
 		}
 		for _, pos := range removable {
-			r := int(real(pos))
-			c := int(imag(pos))
-			lines[r][c] = '.'
+			lines[pos] = '.'
 		}
 	}
 }
