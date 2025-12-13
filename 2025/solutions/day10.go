0485dc6cdd82fc9a94b0857d6b155e9bb283643a
Sean Malloy <sfmalloy@outlook.com>
2025-12-12 21:56:53 -0500

    feat(2025): slow dijkstra's implementation

--- 2025/solutions/day10.go
+++ 2025/solutions/day10.go
@@ -1,7 +1,9 @@
 package solutions
 
 import (
+	"container/heap"
 	"container/list"
+	"fmt"
 	"io"
 	"os"
 	"strconv"
@@ -84,9 +86,14 @@ func (d Day10) Parse(file *os.File, part int) ([]ButtonData, error) {
 }
 
 type ButtonNode[T int | []int] struct {
-	presses      int
-	state        T
-	pressedIndex int
+	presses int
+	state   T
+}
+
+type CounterNode struct {
+	presses *int
+	state   []int
+	goal    *[]int
 }
 
 func (d Day10) Part1(data []ButtonData) int {
@@ -103,7 +110,12 @@ func (d Day10) Part1(data []ButtonData) int {
 }
 
 func (d Day10) Part2(data []ButtonData) int {
-	return 0
+	presses := 0
+	for _, machine := range data {
+		presses += search(machine)
+	}
+
+	return presses
 }
 
 func lightSearch(machine ButtonData, res chan int) {
@@ -119,4 +131,95 @@ func lightSearch(machine ButtonData, res chan int) {
 			q.PushBack(ButtonNode[int]{presses: curr.presses + 1, state: curr.state ^ button})
 		}
 	}
+}
+
+// See IntHeap example: https://pkg.go.dev/container/heap
+
+type CounterHeap []CounterNode
+
+func (h CounterHeap) Len() int {
+	return len(h)
+}
+
+func (h CounterHeap) Less(i, j int) bool {
+	a := distanceToEnd(h[i])
+	b := distanceToEnd(h[j])
+	return a < b
+}
+
+func (h CounterHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *CounterHeap) Push(x any) {
+	*h = append(*h, x.(CounterNode))
+}
+
+func (h *CounterHeap) Pop() any {
+	old := *h
+	N := len(old)
+	val := old[N-1]
+	*h = old[0 : N-1]
+	return val
+}
+
+func search(machine ButtonData) int {
+	dists := map[string]*int{}
+
+	q := &CounterHeap{}
+	heap.Init(q)
+	first := CounterNode{presses: new(int), state: make([]int, len(machine.endCounters)), goal: &machine.endCounters}
+	dists[encode(first.state)] = first.presses
+	heap.Push(q, first)
+
+	for q.Len() > 0 {
+		curr := heap.Pop(q).(CounterNode)
+		fmt.Println(q.Len())
+		encodedState := encode(curr.state)
+		for _, button := range machine.buttons {
+			node := CounterNode{state: make([]int, len(machine.endCounters)), goal: &machine.endCounters}
+			copy(node.state, curr.state)
+			index := 0
+			valid := true
+			for button > 0 {
+				if button&1 == 1 {
+					node.state[index] += 1
+					valid = valid && node.state[index] <= machine.endCounters[index]
+				}
+				button >>= 1
+				index += 1
+			}
+			if !valid {
+				continue
+			}
+			nodeEncode := encode(node.state)
+			currDist, visited := dists[nodeEncode]
+			if !visited {
+				n := new(int)
+				dists[nodeEncode] = n
+				*dists[nodeEncode] = *dists[encodedState] + 1
+				node.presses = n
+				heap.Push(q, node)
+			} else if *currDist < *dists[nodeEncode] {
+				dists[nodeEncode] = currDist
+			}
+		}
+	}
+	return *dists[encode(machine.endCounters)]
+}
+
+func encode(a []int) string {
+	encoded := ""
+	for _, x := range a {
+		encoded += strconv.Itoa(x)
+	}
+	return encoded
+}
+
+func distanceToEnd(n CounterNode) int {
+	dist := 0
+	for i := range len(n.state) {
+		dist += (*n.goal)[i] - n.state[i]
+	}
+	return dist
 }
